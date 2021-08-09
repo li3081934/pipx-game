86370519b59ca07218219feb16c8dae4180363dc
jieshuang.li <lijs8@lenovo.com>
2021-08-09 14:18:48 +0800

    feat: 添加攻击块

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const bump = new Bump(PIPX)
 const app = new Application({ width: 512, height: 512, antialias: true, transparent: false, resolution: 1 })
 const loader = new Loader()
 loader
-    .add(['/assets/texture/player.png', '/assets/texture/wall.png'])
-    .load(setup)
+  .add(['/assets/texture/player.png', '/assets/texture/wall.png', '/assets/texture/attack.png' ])
+  .load(setup)
 
 class Block {
   sprite: PIPX.Sprite;
@@ -19,11 +19,34 @@ class Block {
   }
 }
 
+class Weapon extends Block{
+  owner: Block;
+  status: 'idle'|'hitting' = 'idle';
+  constructor(texture: PIPX.Texture, x: number, y: number, owner: Block, canHit = true) {
+    super(texture, x, y, canHit);
+    this.owner = owner;
+  }
+  hit() {
+    this.status = 'hitting';
+    this.sprite.x = this.owner.sprite.x + 20;
+    this.sprite.y = this.owner.sprite.y + 16;
+    setTimeout(() => {
+      this.hitCancel();
+    }, 200)
+  }
+  hitCancel() {
+    this.status = 'idle';
+    app.stage.removeChild(this.sprite);
+  }
+}
+
 class Player extends Block{
   vx = 0;
   vy = 0;
+  weapon: Weapon;
   constructor(texture: PIPX.Texture, x: number, y: number) {
     super(texture, x, y);
+    this.weapon = new Weapon(loader.resources['/assets/texture/attack.png'].texture, x + 20, y + 16, this);
   }
   move() {
     this.sprite.x += this.vx;
@@ -32,6 +55,10 @@ class Player extends Block{
   stop() {
     this.vx = 0;
     this.vy = 0;
+  }
+  attack() {
+    app.stage.addChild(this.weapon.sprite);
+    this.weapon.hit();
   }
 }
 
@@ -50,10 +77,16 @@ function setup() {
   })
   const player = new Player(textureStore, 68, 50);
   app.stage.addChild(player.sprite);
-  const left = keyboard(37),
-    up = keyboard(38),
-    right = keyboard(39),
-    down = keyboard(40);
+  const left = keyboard(37);
+  const up = keyboard(38);
+  const right = keyboard(39);
+  const down = keyboard(40);
+  const j = keyboard(74);
+  j.press = () => {
+    if (player.weapon.status === 'idle') {
+      player.attack();
+    }
+  };
   down.press = () => {
     player.vy = 1.5;
     player.vx = 0;
@@ -93,47 +126,52 @@ function setup() {
   app.ticker.add(delta => gameLoop(delta));
   function gameLoop(delta) {
     player.move();
-    walls.forEach(wall => {
+    walls.forEach((wall, index) => {
       bump.hit(player.sprite, wall.sprite, true);
+      if (player.weapon.status === 'hitting' && bump.hitTestRectangle(player.weapon.sprite, wall.sprite)) {
+        app.stage.removeChild(wall.sprite);
+        walls.splice(index, 1);
+      }
     })
+
   }
 }
 
 
 function keyboard(keyCode) {
-    let key: any = {};
-    key.code = keyCode;
-    key.isDown = false;
-    key.isUp = true;
-    key.press = undefined;
-    key.release = undefined;
-    //The `downHandler`
-    key.downHandler = event => {
-        if (event.keyCode === key.code) {
-            if (key.isUp && key.press) key.press();
-            key.isDown = true;
-            key.isUp = false;
-        }
-        event.preventDefault();
-    };
+  let key: any = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = event => {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  };
 
-    //The `upHandler`
-    key.upHandler = event => {
-        if (event.keyCode === key.code) {
-            if (key.isDown && key.release) key.release();
-            key.isDown = false;
-            key.isUp = true;
-        }
-        event.preventDefault();
-    };
+  //The `upHandler`
+  key.upHandler = event => {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  };
 
-    //Attach event listeners
-    window.addEventListener(
-        "keydown", key.downHandler.bind(key), false
-    );
-    window.addEventListener(
-        "keyup", key.upHandler.bind(key), false
-    );
-    return key;
+  //Attach event listeners
+  window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+  );
+  window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+  );
+  return key;
 }
 document.body.appendChild(app.view)
